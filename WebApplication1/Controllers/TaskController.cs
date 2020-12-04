aafda5b50086f231de3cbbb41c1517656a67b6e0
unbeat <vandanagupta1499@gmail.com>
2020-12-04 19:14:26 +0530

    Add files via upload

---
+++ WebApplication1/Controllers/TaskController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Model;
+using System.Threading.Tasks;
+using BusinessLogic;
+
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : Controller
+    {
+
+        private TaskOperation taskOpObj;
+        public TaskController()
+        {
+            taskOpObj = new TaskOperation();
+        }
+       
+
+        [HttpGet]
+        public string get()
+        {
+            return "Welcome to Todo Application";
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public string getTask()
+        {
+            return "Hello";
+            //return id.Value.ToString();
+        }
+
+      
+    }
+}
