210d42ac3d56933f5c7d6b51dc515aca14fd593e
unbeat <vandanagupta1499@gmail.com>
2020-12-04 19:13:18 +0530

    Add files via upload

---
+++ Model/Tasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+   public class Tasks
+    {
+        public List<String> TaskList { get; set; }
+        public string Name { get; set; }
+        public int TaskCount { get; set; }
+    }
+
+}
