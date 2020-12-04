68914e622e4c59dd9a120153f0b577f31dc32aa2
unbeat <vandanagupta1499@gmail.com>
2020-12-04 19:12:03 +0530

    Add files via upload

---
+++ BusinessLogic/ITaskOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+namespace BusinessLogic
+{
+    interface ITaskOperation
+    {
+        void AddTask(Tasks obj);
+        void ShowTask(Tasks obj);
+        void DeleteTask(Tasks obj);
+        void EditTask(Tasks obj);
+    }
+}
